2109cd0760208cbfbb162fec805f357b5f963c7d
Guilherme Diniz <guilhermedinizlau@gmail.com>
2023-11-12 14:30:00 -0300

    Remover itens da lista

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const btnEnviar = document.querySelector('#btnEnviar')
 const iptConteudo = document.querySelector('#iptConteudo')
 const listaItens = document.querySelector('#listaItens')
 
-const itens = !window.localStorage.getItem('itens') ? [] : window.localStorage.getItem('itens').split(',')
+let itens = !window.localStorage.getItem('itens') ? [] : window.localStorage.getItem('itens').split(',')
 
 btnEnviar.addEventListener('click', adicionarItem) 
 window.addEventListener('load', renderizarItens)
@@ -10,7 +10,7 @@ window.addEventListener('load', renderizarItens)
 function adicionarItem() {  
     if(iptConteudo.value.trim() === "")  return
 
-    itens.push(iptConteudo.value)
+    itens.unshift(iptConteudo.value)
 
     iptConteudo.value = ''
     iptConteudo.focus()
@@ -23,10 +23,26 @@ function renderizarItens() {
     listaItens.innerHTML = ''
     for(let i = 0; i < itens.length; ++i) {
         const itemLista = document.createElement('li')
+        itemLista.setAttribute('itemId', i)
+
         itemLista.classList.add('item-lista')
+
         itemLista.innerHTML = itens[i]
+
+        itemLista.onclick = aoClicarEm
         listaItens.appendChild(itemLista)
     }
+}
+
+function aoClicarEm(event) {
+    const element = event.target
+    removerItem(element.attributes.itemId.value)
+}
+
+function removerItem(id) {
+    itens.splice(id, 1)
+    salvarLocalStorage()
+    renderizarItens()
 }
 
 function salvarLocalStorage() {
