aada6e25cf273b0b80308910756ed3face48cd5d
Guilherme Diniz <guilhermedinizlau@gmail.com>
2023-11-12 12:27:25 -0300

    Salvando no LocalStorage

--- src/app.js
+++ src/app.js
@@ -2,16 +2,33 @@ const btnEnviar = document.querySelector('#btnEnviar')
 const iptConteudo = document.querySelector('#iptConteudo')
 const listaItens = document.querySelector('#listaItens')
 
+const itens = !window.localStorage.getItem('itens') ? [] : window.localStorage.getItem('itens').split(',')
+
 btnEnviar.addEventListener('click', adicionarItem) 
+window.addEventListener('load', renderizarItens)
 
 function adicionarItem() {  
-    if(iptConteudo.value.trim() === "") 
-        return
+    if(iptConteudo.value.trim() === "")  return
 
-    const itemLista = document.createElement('li')
-    itemLista.classList.add('item-lista')
-    itemLista.innerHTML = iptConteudo.value
-    listaItens.appendChild(itemLista)
+    itens.push(iptConteudo.value)
+
     iptConteudo.value = ''
     iptConteudo.focus()
+
+    salvarLocalStorage()
+    renderizarItens()
+}
+
+function renderizarItens() {
+    listaItens.innerHTML = ''
+    for(let i = 0; i < itens.length; ++i) {
+        const itemLista = document.createElement('li')
+        itemLista.classList.add('item-lista')
+        itemLista.innerHTML = itens[i]
+        listaItens.appendChild(itemLista)
+    }
+}
+
+function salvarLocalStorage() {
+    window.localStorage.setItem('itens', itens)
 }
